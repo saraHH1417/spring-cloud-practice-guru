41b29ad7773135e486038d2de2bc54d86ea8b01a
saraHH1417 <sara.afzar@gmail.com>
2023-02-18 01:30:16 +0330

    feat: initial beer service

---
+++ mssc-beer/src/test/java/practice/sara/msscbeer/MsscBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.sara.msscbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
