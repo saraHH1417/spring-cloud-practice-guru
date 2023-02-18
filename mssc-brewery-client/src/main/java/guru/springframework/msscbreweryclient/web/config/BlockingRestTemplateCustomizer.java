5ed9201d631a12201fba36b43575ede1e3eedb1b
saraHH1417 <sara.afzar@gmail.com>
2023-02-18 18:31:15 +0330

    feat: http config

---
+++ mssc-brewery-client/src/main/java/guru/springframework/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -0,0 +1,62 @@
+package guru.springframework.msscbreweryclient.web.config;
+
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Created by jt on 2019-08-08.
+ */
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    private final Integer maxTotalConnection;
+    private final Integer defaultMaxTotalConnction;
+    private final Integer connectionRequestTimeout;
+    private final Integer socketTimeout;
+
+    public BlockingRestTemplateCustomizer(
+            @Value("${sfg.maxtotalconnections}") Integer maxTotalConnection,
+            @Value("${sfg.defaultmaxtotalconnectiion}") Integer defaultMaxTotalConnction,
+            @Value("${sfg.connectionrequesttimeout}") Integer connectionRequestTimeout,
+            @Value("${sfg.sockettimeout}") Integer socketTimeout) {
+        this.maxTotalConnection = maxTotalConnection;
+        this.defaultMaxTotalConnction = defaultMaxTotalConnction;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.socketTimeout = socketTimeout;
+    }
+
+    public ClientHttpRequestFactory clientHttpRequestFactory(){
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(maxTotalConnection);
+        connectionManager.setDefaultMaxPerRoute(defaultMaxTotalConnction);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    }
+}
