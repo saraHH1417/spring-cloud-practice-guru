f200306c1450bfc340ca5da8ce46068ef4d34c4d
saraHH1417 <sara.afzar@gmail.com>
2023-02-18 16:39:30 +0330

    feat: create get request

---
+++ mssc-brewery-client/src/main/java/guru/springframework/msscbreweryclient/web/client/BreweryClient.java
@@ -0,0 +1,36 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+//@ConfigurationProperties(prefix= "sfg.brewery" , ignoreInvalidFields = false)
+public class BreweryClient {
+
+    public final String BEER_PATH_V1 = "/api/v1/beer/";
+    @Value("${sfg.brewery.apihost}")
+    private String apihost;
+    private final RestTemplate restTemplate;
+
+    public BreweryClient(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplate = restTemplateBuilder.build();
+    }
+
+    public BeerDto getBeerById(UUID uuid) {
+        return restTemplate.getForObject(apihost + BEER_PATH_V1 + uuid.toString(),
+                                        BeerDto.class);
+    }
+
+    private void setApiHost(String apiHost) {
+        this.apihost = apiHost;
+    }
+
+
+}
