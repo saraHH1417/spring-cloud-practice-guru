d258788eaf173f88f58151a16cf4849581e64680
saraHH1417 <sara.afzar@gmail.com>
2023-02-18 18:41:19 +0330

    feat: create beer domain

---
+++ mssc-beer/src/main/java/practice/sara/msscbeer/domain/Beer.java
@@ -0,0 +1,46 @@
+package practice.sara.msscbeer.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.context.annotation.Primary;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    private Timestamp lastModifiedDate;
+    private String beerName;
+    private String beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+
+
+}
